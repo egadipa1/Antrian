4c91ef4657d479a6349a6de53df96235dedf6e1f
Ega Dipa Nusantara <dipanusantaraega@gmail.com>
2025-03-03 14:27:28 +0700

    Migrate

--- database/migrations/2025_03_03_054432_create_pasiens_table.php
+++ database/migrations/2025_03_03_054432_create_pasiens_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('pasiens', function (Blueprint $table) {
             $table->id('ID_Pasien');
             $table->string('Nama_Pasien');
+            $table->string('NIK');
             $table->date('Tanggal_Lahir');
             $table->string('Jenis_Kelamin', 10);
             $table->string('Alamat');
