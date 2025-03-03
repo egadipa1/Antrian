bed031d1fc5d3261422533b15bf9098a73efad30
Ega Dipa Nusantara <dipanusantaraega@gmail.com>
2025-03-03 18:59:27 +0700

    add component

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,9 @@
+<div class="bg-primary bg-opacity-75 navbar-expand-lg">
+    <div class="container-fluid">
+        <div class="flex justify-content-beetwen m-x-2">
+            <div>
+
+            </div>
+        </div>
+    </div>
+</div>
