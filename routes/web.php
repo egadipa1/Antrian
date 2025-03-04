26d09fe4e92bebf82ab6bdaaa3d6f54bbd558eb7
Ega Dipa Nusantara <dipanusantaraega@gmail.com>
2025-03-04 08:02:04 +0700

    home Page

--- routes/web.php
+++ routes/web.php
@@ -7,5 +7,5 @@ Route::get('create', [AntrianController::class, 'create'])->name('create');
 Route::post('store', [AntrianController::class, 'store'])->name('store');
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
